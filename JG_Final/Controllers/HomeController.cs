393609dcbe6096e1e27897314d8c461fecd8bba7
goyaschooling <goyaschooling@gmail.com>
2017-12-21 23:40:15 -0700

    add all files

---
+++ JG_Final/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using JG_Final.Models;
+using PerpetuumSoft.Knockout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JG_Final.Controllers
+{
+
+    public class HomeController : Controller
+    //public class FooController : KnockoutController
+    {   
+        // GET: Home              
+        public ActionResult Index()
+        {
+            
+            InitializeViewBag("Better list");
+            var model = new BetterListModel
+            {
+                AllItems = new List<string> { "Fries", "Eggs Benedict", "Ham", "Cheese" },
+                SelectedItems = new List<string> { "Ham" }
+            };
+            return View(model);
+            //return View();
+        }
+
+        private void InitializeViewBag(string v)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public ActionResult AddItem(BetterListModel model)
+        {
+            model.AddItem();
+            return Json(model);
+        }
+
+        public ActionResult RemoveSelected(BetterListModel model)
+        {
+            model.RemoveSelected();
+            return Json(model);
+        }
+
+        public ActionResult SortItems(BetterListModel model)
+        {
+            model.SortItems();
+            return Json(model);
+        }
+
+
+    }
+}
---
+++ JG_Final/Models/List.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JG_Final.Models
+{
+    public class BetterListModel
+    {
+        public string ItemToAdd { get; set; }
+        public List<string> AllItems { get; set; }
+        public List<string> SelectedItems { get; set; }
+
+        public void AddItem()
+        {
+            if (!string.IsNullOrEmpty(ItemToAdd) && !AllItems.Contains(ItemToAdd))
+                AllItems.Add(ItemToAdd);
+            ItemToAdd = "";
+        }
+
+        public void RemoveSelected()
+        {
+            AllItems.RemoveAll(item => SelectedItems.Contains(item));
+            SelectedItems.Clear();
+        }
+
+        public void SortItems()
+        {
+            AllItems.Sort();
+        }
+    }
+}
